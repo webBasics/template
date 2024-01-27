4c3778309585b183dbfbec2ea0f0f8541b6c2694
WebBasics <96490710+webBasics@users.noreply.github.com>
2024-01-27 18:21:32 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const button = document.querySelector('button');
 button.addEventListener('click', ()=>{
   Notification.requestPermission().then(perm => {
     if(perm === 'granted'){
-      console.log('Hello')
+      const notification = new Notification("Hello!", {
+        body: "This is the body",
+        data: { data : 'content'},
+        //icon: "img/... .jpg",
+        //tag: "Hello Notification", //prevent stacking
+      });
+
+      notification.addEventListener('close', e=>{
+        console.log('Closed');
+      });
     }
   });
 });
